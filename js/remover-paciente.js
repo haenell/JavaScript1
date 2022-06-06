4ff1455ee28130619edc77f22db058e4a3e6c3e8
Haenell Farias <haenellfilho@hotmail.com>
2022-06-06 15:22:48 -0300

    atualizações gerais

---
+++ js/remover-paciente.js
@@ -0,0 +1,8 @@
+var pacientes = document.querySelectorAll('.paciente')
+console.log(pacientes)
+
+pacientes.forEach(function (paciente) {
+  paciente.addEventListener('dblclick', function () {
+    this.remove()
+  })
+})
